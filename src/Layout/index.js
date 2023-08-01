50a44da0b322269cdc82c53f1ac8a1f8a66f0934
bushra <bushra11771@gmail.com>
2023-08-02 03:40:42 +0500

    Add svg icon

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -9,7 +9,7 @@ const Layout = () => {
       <Header />
       <SideBar />
       <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+        <div className=" rounded-lg dark:border-gray-700 mt-4">
           <Outlet />
         </div>
       </div>
